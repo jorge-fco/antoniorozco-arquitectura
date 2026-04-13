67db10a86eadda93327a433576716b0f887b1a1a
Jorge Fco. <jorge.fco.mend@gmail.com>
2026-04-13 09:35:33 -0600

    Update home

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+/*
+	APP
+	Jorge Fco.
+	20206
+*/
+(function($){
+	'use strict';
+
+	const APP = {
+		init: function(){
+			
+		}
+	};
+
+	// Run..
+	window.theme = APP;
+	window.onload = function() {
+		APP.init();
+	};
+
+})(jQuery);
