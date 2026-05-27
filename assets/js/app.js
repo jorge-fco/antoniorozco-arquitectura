2ce76b36427af8e994e5266375c1a4a2ab6b1eaf
Jorge Fco. <jorge.fco.mend@gmail.com>
2026-05-27 09:49:22 -0600

    Add new pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,28 @@
 /*
 	APP
 	Jorge Fco.
-	20206
+	2026
 */
 (function($){
 	'use strict';
 
 	const APP = {
 		init: function(){
-			
-		}
+			APP.Header();
+		},
+		Header: function(){
+			const Header = document.getElementById("header");
+			var $Header = $('.js-header');
+
+			// Check
+			if ($('.js-home').length) {
+				Header.style.mixBlendMode = "none";
+			} else {
+				$Header.addClass('is--dark');
+				Header.style.mixBlendMode = "difference";
+
+			}
+		},
 	};
 
 	// Run..
